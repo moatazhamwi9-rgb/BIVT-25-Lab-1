b37820668aae89536339de8255510929f1a305a9
Миша <moatazhamwi9@gmail.com>
2025-09-16 13:43:14 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -6,82 +6,61 @@
         {
             bool answer = false;
 
-            // code here
-
-            // end
+            if (b != 0 && a % b == 0)
+                answer = true;
 
             return answer;
         }
+
         public bool Task2(double d)
         {
             bool answer = false;
 
-            // code here
-
-            // end
+            if (d > 1 || d < 0)
+                answer = true;
 
             return answer;
         }
+
         public bool Task3(int a, int b)
         {
             bool answer = false;
 
-            // code here
-
-            // end
+            if (a == 0 || b == 0)
+                answer = true;
 
             return answer;
         }
+
         public double Task4(double d, double f, double g)
         {
-            double answer = 0;
-
-            // code here
-
-            // end
-
+            double answer = Math.Max(d, Math.Max(f, g));
             return answer;
         }
+
         public double Task5(double x)
         {
-            double answer = 0;
-
-            // code here
-
-            // end
-
+            double answer = (x < 0) ? -x : 1;
             return answer;
         }
+
         public bool Task6(double circleS, double squareS)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
+            bool answer = circleS <= squareS;
             return answer;
         }
 
         public double Task7(double d, double f)
         {
-            int answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
+            if (d > f) return 1;
+            else if (d < f) return -1;
+            else return 0;
         }
+
         public bool Task8(int a, int b, int c)
         {
-            bool answer = false;
-
-            // code here
-            
-            // end
-
+            bool answer = (a + b > c) && (a + c > b) && (b + c > a);
             return answer;
         }
     }
-}
+ }
