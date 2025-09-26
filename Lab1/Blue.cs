be9516004bd4b6ef2c0191bd782811b08bfcc897
Миша <moatazhamwi9@gmail.com>
2025-09-26 14:42:10 +0300

    New Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,87 +1,61 @@
-﻿namespace Lab1
+using System;
+
+namespace Lab1
 {
     public class Blue
     {
+        // Task1: Check if both numbers are positive
         public bool Task1(int a, int b)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            return a > 0 && b > 0;
         }
+
+        // Task2: Check if number is between 0 and 1 (exclusive)
         public bool Task2(double d)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            return d > 0 && d < 1;
         }
+
+        // Task3: Check if both numbers are odd
         public bool Task3(int a, int b)
         {
-            bool answer = false;
-
-            // code here
-
-            // end
-
-            return answer;
+            return a % 2 != 0 && b % 2 != 0;
         }
+
+        // Task4: Return the maximum of three numbers
         public double Task4(double d, double f, double g)
         {
-            double answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
+            return Math.Max(d, Math.Max(f, g));
         }
+
+        // Task5: Return |x| if x < 0, otherwise return x^2
         public double Task5(double x)
         {
-            double answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
+            return x < 0 ? Math.Abs(x) : x * x;
         }
+
+        // Task6: Check if a circle with area circleS can fit inside a square with area squareS
         public bool Task6(double circleS, double squareS)
         {
-            bool answer = false;
+            // Circle radius
+            double r = Math.Sqrt(circleS / Math.PI);
+            // Square side
+            double side = Math.Sqrt(squareS);
 
-            // code here
-
-            // end
-
-            return answer;
+            // Circle fits inside square if diameter <= side
+            return 2 * r <= side;
         }
 
+        // Task7: Return the smaller of two numbers
         public double Task7(double d, double f)
         {
-            int answer = 0;
-
-            // code here
-
-            // end
-
-            return answer;
+            return Math.Min(d, f);
         }
+
+        // Task8: Check if triangle with sides a, b, c is valid
         public bool Task8(int a, int b, int c)
         {
-            bool answer = false;
-
-            // code here
-            
-            // end
-
-            return answer;
+            return a + b > c && a + c > b && b + c > a;
         }
     }
 }
